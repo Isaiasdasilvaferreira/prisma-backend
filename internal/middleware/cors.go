38bc467655fb87fac27718ad254389d36abc41b9
Isaias da Silva Ferreira <isaiasdasilvaf52@gmail.com>
2026-07-03 12:59:25 -0300

    Update cors.go

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "https://prisma-frontend-eight.vercel.app")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Expose-Headers", "Authorization")
