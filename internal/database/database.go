7acf5bc4226bf8661b626129f4925a39f7c1cf85
Isaias da Silva Ferreira <isaiasdasilvaf52@gmail.com>
2026-07-04 13:17:34 -0300

    Update database.go

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,3 +1,18 @@
 package database
 
-// TODO: implement database connection
+import (
+	"github.com/Isaiasdasilvaferreira/prisma-backend/internal/config"
+	"github.com/nedpals/supabase-go"
+)
+
+type Database struct {
+	Supabase *supabase.Client
+}
+
+func NewDatabase(cfg *config.Config) (*Database, error) {
+	supabaseClient := supabase.CreateClient(cfg.SupabaseURL, cfg.SupabaseAnonKey)
+
+	return &Database{
+		Supabase: supabaseClient,
+	}, nil
+}
