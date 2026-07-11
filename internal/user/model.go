b154132ee26e14c089636733864e22a5a71be6a0
Isaias da Silva Ferreira <isaiasdasilvaf52@gmail.com>
2026-07-11 19:20:47 -0300

    Update model.go

--- internal/user/model.go
+++ internal/user/model.go
@@ -1,3 +1,42 @@
 package user
 
-// TODO: define user models
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type PlanType string
+
+const (
+	PlanFree        PlanType = "free"
+	PlanProfessional PlanType = "professional"
+)
+
+type UserPlan struct {
+	ID        uuid.UUID `json:"id"`
+	UserID    uuid.UUID `json:"user_id"`
+	PlanType  PlanType  `json:"plan_type"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type DailyUsage struct {
+	ID         uuid.UUID `json:"id"`
+	UserID     uuid.UUID `json:"user_id"`
+	UsageDate  string    `json:"usage_date"`
+	UsageCount int       `json:"usage_count"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
+}
+
+func (p PlanType) GetDailyLimit() int {
+	switch p {
+	case PlanProfessional:
+		return 30
+	case PlanFree:
+		return 10
+	default:
+		return 10
+	}
+}
