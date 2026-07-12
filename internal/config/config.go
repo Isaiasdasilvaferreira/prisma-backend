086a7be09bc846bac0a148f3e1e3e97144734fa6
Isaias da Silva Ferreira <isaiasdasilvaf52@gmail.com>
2026-07-12 13:32:58 -0300

    Update config.go

--- internal/config/config.go
+++ internal/config/config.go
@@ -5,18 +5,20 @@ import (
 )
 
 type Config struct {
-	Port              string
-	SupabaseURL       string
-	SupabaseAnonKey   string
-	SupabaseJWTSecret string
+	Port                   string
+	SupabaseURL            string
+	SupabaseAnonKey        string
+	SupabaseJWTSecret      string
+	SupabaseServiceRoleKey string
 }
 
 func LoadConfig() *Config {
 	return &Config{
-		Port:              getEnv("PORT", "8080"),
-		SupabaseURL:       getEnv("SUPABASE_URL", ""),
-		SupabaseAnonKey:   getEnv("SUPABASE_ANON_KEY", ""),
-		SupabaseJWTSecret: getEnv("SUPABASE_JWT_SECRET", ""),
+		Port:                   getEnv("PORT", "8080"),
+		SupabaseURL:            getEnv("SUPABASE_URL", ""),
+		SupabaseAnonKey:        getEnv("SUPABASE_ANON_KEY", ""),
+		SupabaseJWTSecret:      getEnv("SUPABASE_JWT_SECRET", ""),
+		SupabaseServiceRoleKey: getEnv("SUPABASE_SERVICE_ROLE_KEY", ""),
 	}
 }
 
