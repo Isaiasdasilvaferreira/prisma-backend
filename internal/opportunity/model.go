b1adff1abdb6f704b13202f4d2b709d81b95568c
Isaias da Silva Ferreira <isaiasdasilvaf52@gmail.com>
2026-07-12 19:56:12 -0300

    Update model.go

--- internal/opportunity/model.go
+++ internal/opportunity/model.go
@@ -35,7 +35,6 @@ type Opportunity struct {
 	Location       string       `json:"location"`
 	ApplicationURL string       `json:"application_url"`
 	IsActive       bool         `json:"is_active"`
-	UserID         string       `json:"user_id"`
 	CreatedAt      time.Time    `json:"created_at"`
 	UpdatedAt      time.Time    `json:"updated_at"`
 }
