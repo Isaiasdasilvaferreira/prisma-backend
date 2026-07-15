a3554104845ab1e4c1900bdb0035c92ba56aa563
Isaias da Silva Ferreira <isaiasdasilvaf52@gmail.com>
2026-07-14 22:21:33 -0300

    Update dto.go

--- internal/user_opportunity/dto.go
+++ internal/user_opportunity/dto.go
@@ -3,22 +3,22 @@ package user_opportunity
 import "time"
 
 type UserOpportunityResponse struct {
-	ID                    string    `json:"id"`
-	Title                 string    `json:"title"`
-	Company               string    `json:"company"`
-	ContractType          string    `json:"contract_type"`
-	Modality              string    `json:"modality"`
-	Location              *string   `json:"location"`
-	Salary                *string   `json:"salary"`
-	AvailableRegistration *int      `json:"available_registration"`
-	WhatsApp              *string   `json:"whatsapp"`
-	Email                 string    `json:"email"`
-	Description           string    `json:"description"`
-	Responsibilities      *string   `json:"responsibilities"`
-	Requirements          *string   `json:"requirements"`
-	IsActive              bool      `json:"is_active"`
-	CreatedAt             time.Time `json:"created_at"`
-	UpdatedAt             time.Time `json:"updated_at"`
+	ID                     string     `json:"id"`
+	Title                  string     `json:"title"`
+	Company                string     `json:"company"`
+	ContractType           string     `json:"contract_type"`
+	Modality               string     `json:"modality"`
+	Location               *string    `json:"location"`
+	Salary                 *string    `json:"salary"`
+	AvailableRegistration  *int       `json:"available_registration"`
+	WhatsApp               *string    `json:"whatsapp"`
+	Email                  string     `json:"email"`
+	Description            string     `json:"description"`
+	Responsibilities       *string    `json:"responsibilities"`
+	Requirements           *string    `json:"requirements"`
+	IsActive               bool       `json:"is_active"`
+	CreatedAt              time.Time  `json:"created_at"`
+	UpdatedAt              time.Time  `json:"updated_at"`
 }
 
 func (u *UserOpportunity) ToResponse() UserOpportunityResponse {
